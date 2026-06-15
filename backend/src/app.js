7ac5b25e33f85372a02aabdf3d4e4ed037160b86
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2026-06-14 19:12:23 -0500

    Modificacion del vercel.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,9 @@ app.use('/api/categories', categoryRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 module.exports = app;
